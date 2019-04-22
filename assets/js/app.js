4293fbb338db41bec5552c7ef9689f39e29fc895
Layla Jooya <laylajooya@gmail.com>
2019-04-22 18:08:01 -0500

    added spotify login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
+
+// SETLISTFM API - SEARCH ARTIST AND GET SETLIST DATE/LOCATION/AND VIEW
+
 $("#submitPress").on("click", function(event) {
     event.preventDefault();
     var artistName = $("#user-input").val();
     searchSetlistFM(artistName);
 });
-
-
 
 function searchMBID(mbid) {
     var originalURL = "https://api.setlist.fm/rest/1.0/search/setlists?artistMbid=" + mbid.replace(/\"/g, "") + "&p=1";
@@ -30,7 +31,6 @@ function searchMBID(mbid) {
         var button = $(
             `<button data-url=${(artistSetlists[i].url)}>View</button>`
         );
-
         $("#setlist-results").append(dateText, citystateText, button)
  
         // $("#setlist-results").append(button)
@@ -39,7 +39,7 @@ function searchMBID(mbid) {
 
     });
   
-}
+};
 
 function searchSetlistFM(artistName) {
     var originalURL = "https://api.setlist.fm/rest/1.0/search/artists?artistName=" + artistName + "&p=1&sort=relevance"
@@ -63,3 +63,53 @@ function searchSetlistFM(artistName) {
       console.error(textStatus)
     })
   };
+
+
+// SPOTIFY API
+
+// Get the hash of the url
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = 'abf37ff11aff48afb9ba75f4debfc293';
+const redirectUri = 'http://localhost:8888/callback';
+const scopes = [
+  'user-top-read'
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+    $("#login").on("click", function(event){
+        event.preventDefault
+        window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+});
+
+// Make a call using the token
+$.ajax({
+   url: "https://api.spotify.com/v1/me/top/artists",
+   type: "GET",
+   beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+   success: function(data) { 
+     // Do something with the returned data
+     data.items.map(function(artist) {
+       let item = $('<li>' + artist.name + '</li>');
+       item.appendTo($('#top-artists'));
+     });
+   }
+});
+}
