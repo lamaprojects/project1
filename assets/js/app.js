96e937258bbbe4a514054411aec314443cef1e24
Layla Jooya <laylajooya@gmail.com>
2019-04-26 11:07:01 -0500

    making playlists folks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
+//html loading before js runs
 $(document).ready(function() {
   console.log("ready!");
   var user = "";
   var songs = [];
+
   //THESE ARE HELPER FUNCTIONS TO BREAK DOWN THE URL AND SAVE THE ACCESS TOKEN
   function getParameterByName(name) {
     var match = RegExp("[#&]" + name + "=([^&]*)").exec(window.location.hash);
@@ -34,37 +36,12 @@ $(document).ready(function() {
     }
   });
 
-  // function createPlaylist(callback) {
-  //   //THIS IS AN IIFE (YOU TO FILL THIS IN)
-  //   console.log("creating a playlist");
-  //   $.ajax({
-  //       method: 'POST',
-  //       url: `https://api.spotify.com/v1/users/laylajoo/playlists`,
-  //       headers: {
-  //         Authorization: "Bearer " + localStorage.getItem("access_token")
-  //       },
-  //       data: JSON.stringify({
-  //         'name': name,
-  //         'public': false
-  //       }),
-  //       success: function(msg){
-  //           console.log("Successfully saved a badass playlist about retrieving data", msg)
-  //       }
-  //   });
-  // }
-  // createPlaylist()
-
-  //DIscover more queries here: https://github.com/jmperez/spotify-web-api-js
-  // SETLISTFM API - SEARCH ARTIST AND GET SETLIST DATE/LOCATION/AND VIEW
+  // making playlist with Spotify ajax call
   function makePlaylist(access_token, user, songs) {
-    console.log(user);
-    console.log(access_token);
-
-    var urlString =
-      "https://api.spotify.com/v1/users/" + user.id + "/playlists";
+    var urlString = "https://api.spotify.com/v1/users/" + user.id + "/playlists";
 
     var jsonData = {
-      name: "My Fucking Playlist",
+      name: "My SetlistFM Playlist",
       public: false
     };
 
@@ -78,18 +55,32 @@ $(document).ready(function() {
       },
       contentType: "application/json",
       success: function(result) {
-        console.log(result);
-        console.log("Woo");
+        console.log("created playlist");
         
-        // so this is where we are going to perform the search for songs 
         var spotifyPlaylistId = result.id 
-        var urlStringForPlaylist = 'https://api.spotify.com/v1/playlists/' + spotifyPlaylistId + '/tracks'
-      },
-      error: function(error) {
-        console.log(error);
-        console.log("Error");
-      }
-    });
+        var urlStringForPlaylist = 'https://api.spotify.com/v1/playlists/' + spotifyPlaylistId + '/tracks?uris=' + songs.join()
+        $.ajax({
+          type: "POST",
+          url: urlStringForPlaylist,
+          data: JSON.stringify(jsonData),
+          dataType: "json",
+          headers: {
+            Authorization: "Bearer " + access_token
+          },
+          successs: function(result) {
+            console.log("songs were added holy shit!!!")
+          },
+          error: function(error) {
+            console.log(error);
+            console.log("this ERROR happened in making playlist");
+          },
+          error: function(error) {
+            console.log(error);
+            console.log("this ERROR happened in adding songs to playlist");
+          }
+        }); // this closes second ajax call to add songs to playlist
+      }, // this closes the success for 1st ajax call 
+    }); // this closes first ajax call to create playlist
   }
 
   $("#playlist").on("click", function() {
@@ -102,6 +93,7 @@ $(document).ready(function() {
     searchSetlistFM(artistName);
   });
 
+  // search setlistFM for artist mbid
   function searchMBID(mbid) {
     // CORS-anywhere hack - we're doing this instead of creating a server
     var originalURL =
@@ -177,20 +169,15 @@ $(document).ready(function() {
                 },
                 contentType: "application/json",
                 success: function(result) {
-                  console.log(result);
                   spotifyTracksArray.push(result.tracks.items[0].uri)
-                  console.log(spotifyTracksArray);
-                  // now we need to push uri into spotifySongsUrls
                 },
                 error: function(error) {
                   console.log(error);
                   console.log("Error");
                 }
               });
-
             }
-            // makePlaylist(access_token, user, spotifyTracksArray)
-            // console.log('these are the songs i need to search for: ' + $(this).attr('data-songs'))
+            makePlaylist(access_token, user, spotifyTracksArray);
           },
           'data-songs': songs[i]
         });
@@ -228,63 +215,13 @@ $(document).ready(function() {
         "x-requested-with": "xhr",
         "x-api-key": "6d1b43e2-d601-4dee-91e1-9889e57516f7"
       }
-    })
-      .done(function(response) {
-        var artistMBID = JSON.stringify(response.artist[0].mbid);
+    }).done(function(response) {
+      var artistMBID = JSON.stringify(response.artist[0].mbid);
 
-        // This function does the setlist lookup and performs HTML front-end stuff
-        searchMBID(artistMBID);
-      })
-      .fail(function(jqXHR, textStatus) {
-        console.error(textStatus);
-      });
+      // This function does the setlist lookup and performs HTML front-end stuff
+      searchMBID(artistMBID);
+    }).fail(function(jqXHR, textStatus) {
+      console.error(textStatus);
+    });
   }
-
-  // // SPOTIFY API
-
-  // // Get the hash of the url
-  // const hash = window.location.hash
-  // .substring(1)
-  // .split('&')
-  // .reduce(function (initial, item) {
-  //   if (item) {
-  //     var parts = item.split('=');
-  //     initial[parts[0]] = decodeURIComponent(parts[1]);
-  //   }
-  //   return initial;
-  // }, {});
-  // window.location.hash = '';
-
-  // // Set token
-  // let _token = hash.access_token;
-
-  // const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-  // // Replace with your app's client ID, redirect URI and desired scopes
-  // const clientId = 'abf37ff11aff48afb9ba75f4debfc293';
-  // const redirectUri = 'https://reliveapp.github.io/project1/';
-  // const scopes = [
-  //   'user-top-read'
-  // ];
-
-  // // If there is no token, redirect to Spotify authorization
-  // if (!_token) {
-  //     $("#login").on("click", function(event){
-  //         event.preventDefault
-  //         window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
-  // });
-
-  // Make a call using the token
-  // $.ajax({
-  //    url: "https://api.spotify.com/v1/me/top/artists",
-  //    type: "GET",
-  //    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-  //    success: function(data) {
-  //      // Do something with the returned data
-  //      data.items.map(function(artist) {
-  //        let item = $('<li>' + artist.name + '</li>');
-  //        item.appendTo($('#top-artists'));
-  //      });
-  //    }
-  // });
 });
