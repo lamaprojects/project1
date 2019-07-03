c83fbb01de3fc60495ddd111a6b7e9be8c3de9fe
aine <aineekohh@yahoo.com>
2019-07-03 18:13:36 -0500

    tried a different code to make button mobile responsive in app.js, lets see if this works

--- assets/js/app.js
+++ assets/js/app.js
@@ -87,8 +87,8 @@ $(document).ready(function() {
 
  // THE ONLCLICK METHOD WILL TARGET THE SUBMIT BUTTON IN search.html 
   
-  //$("#submitPress").on("click", function(event) {
-    $("#form").find('#submitPress').on("click", function(event) {
+  // $("#submitPress").on("click", function(event) {
+    $(document).on('click', '#submitPress', function() {
     event.preventDefault();
     var artistName = $("#user-input").val();
     searchSetlistFM(artistName);
